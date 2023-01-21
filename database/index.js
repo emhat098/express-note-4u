0af08e7e7003615e5dcf16b0e9fd97fba077d315
Em Ha Tuan <emhat@nashtechglobal.com>
2023-01-21 20:07:51 +0700

    Update building time

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 // app.js
 const postgres = require('postgres');
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
